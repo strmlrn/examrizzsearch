cace7b2fb1626f7c41649c24fb807f6edb1455c7
strmlrn <sj@streamlearning.co>
2024-09-02 22:29:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['https://examrizzsearch.com', 'https://www.examrizzsearch.com'],
+  origin: ['https://examrizzsearch.com', 'https://www.examrizzsearch.com', 'http://www.examrizzsearch.com'],
   credentials: true
 }));
 
