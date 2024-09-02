c0eaf25a5ce656968bec0dbcb5473e1f2c095fcd
strmlrn <sj@streamlearning.co>
2024-09-02 15:25:32 +0100

    Update test-db.js

--- test-db.js
+++ test-db.js
@@ -2,15 +2,13 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const fs = require('fs');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    ca: fs.readFileSync(__dirname + '/server-ca.pem')
-  }
+  connectionLimit: 10
 });
 
 connection.connect((err) => {
